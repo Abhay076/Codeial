498b918cd4c9d5966abe20f676ef6024993bcc31
Abhay 076 <yadavabhaycs@gmail.com>
2022-06-24 08:11:10 +0530

    Create a Controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const homeControllers = require('../controllers/home_controllers');
+
 console.log('Router is loaded');
+
+router.get('/',homeControllers.home);
 
 module.exports =router;

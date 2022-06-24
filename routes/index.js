a9299abcfb441c780ce47175fd680f60f70130c6
Abhay 076 <yadavabhaycs@gmail.com>
2022-06-24 08:40:39 +0530

    Create another Controllers and Routers: Users

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@ const router = express.Router();
 
 const homeControllers = require('../controllers/home_controllers');
 
+
 console.log('Router is loaded');
 
 router.get('/',homeControllers.home);
+
+router.use('/users',require('./users'));
+
+//for any further routes, access from here
+//router.use('/NameofRoutes',requires('./routerFile'));
 
 module.exports =router;

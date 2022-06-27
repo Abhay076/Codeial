75e95c1b257c4a044e9777d9927e244b0a268f0c
Abhay 076 <yadavabhaycs@gmail.com>
2022-06-27 23:47:33 +0530

    User Sign In

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,5 @@ router.get('/profile',usersControllers.profile);
 router.get('/sign-up',usersControllers.signUp);
 router.get('/sign-in',usersControllers.signIn);
 router.post('/create',usersControllers.create);
+router.post('/create-session',usersControllers.createSession);
 module.exports =router;

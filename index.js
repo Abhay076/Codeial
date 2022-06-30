9d198b7a813c7af9d37be8f2eb7e97d50aaa571b
Abhay 076 <yadavabhaycs@gmail.com>
2022-06-30 08:48:08 +0530

    Setting up mongo store for session cookies

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(expressLayouts);
 
@@ -28,7 +29,7 @@ app.set('layout extractScripts',true);
  app.set('view engine','ejs');
  app.set('views','./views');
 
-
+//mongo store is used to store the session cookie in the db
  app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -37,7 +38,16 @@ app.set('layout extractScripts',true);
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl:'mongodb://localhost/codeials_development',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongoDB setup ok');
+        }
+    )
  }));
 
  app.use(passport.initialize());

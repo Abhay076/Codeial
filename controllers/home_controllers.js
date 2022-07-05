93010d0521e3d2ebd24ed4bacfc20d8e7727291f
Abhay 076 <yadavabhaycs@gmail.com>
2022-07-05 11:58:49 +0530

    Show Post in my browsers

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,9 +1,13 @@
-
+const Post = require('../models/post');
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('name_id', 11);
     // res.end('<h1> Express is up for codeial </h1>');
+Post.find({}, function(err,posts){
     return res.render('home',{
-        title:"Home"
+        title:"Codeial | Home",
+        posts: posts
     });
+});
+    
 }

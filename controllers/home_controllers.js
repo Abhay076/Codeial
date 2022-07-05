d25522b5c4ec9eb9a14a0068bddbcca1156dee47
Abhay 076 <yadavabhaycs@gmail.com>
2022-07-05 12:50:02 +0530

    Show user's name on their post using Populate

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -3,7 +3,15 @@ module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('name_id', 11);
     // res.end('<h1> Express is up for codeial </h1>');
-Post.find({}, function(err,posts){
+// Post.find({}, function(err,posts){
+//     return res.render('home',{
+//         title:"Codeial | Home",
+//         posts: posts
+//     });
+// });
+
+//papulate the user of each post
+Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home',{
         title:"Codeial | Home",
         posts: posts

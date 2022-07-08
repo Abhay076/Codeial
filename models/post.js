be3221d10459a91baf64604b0fedd71f0aedfecf
Abhay 076 <yadavabhaycs@gmail.com>
2022-07-08 10:20:07 +0530

    Nested Population :: Display Comments

--- models/post.js
+++ models/post.js
@@ -13,7 +13,7 @@ const postSchema = new mongoose.Schema({
     comments:[
         {
             type: mongoose.Types.ObjectId,
-            ref: 'comment'
+            ref: 'Comment'
         }
     ]
 },{

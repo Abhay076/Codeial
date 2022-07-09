167e2b46eec712b2fb77924e9d1e159d3a3f23fc
Abhay 076 <yadavabhaycs@gmail.com>
2022-07-09 13:42:18 +0530

    Converting to Async Await

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,6 +1,6 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // console.log(req.cookies);
     // res.cookie('name_id', 11);
     // res.end('<h1> Express is up for codeial </h1>');
@@ -11,24 +11,28 @@ module.exports.home = function(req,res){
 //     });
 // });
 
-//papulate the user of each post
-Post.find({})
+try{
+    //papulate the user of each post
+let posts = await Post.find({})
 .populate('user')
 .populate({
     path:'comments',
     populate: {
         path:'user'
     }
-})
-.exec(function(err,posts){
-    User.find({},function(err,users){
-        return res.render('home',{
-            title:"Codeial | Home",
-            posts: posts,
-            all_users: users
-        });
-    });
-    
 });
+ let users = await User.find({});
+
+ return res.render('home',{
+    title:"Codeial | Home",
+    posts: posts,
+    all_users: users
+});
+
+}catch(err){
+    console.log("error", err);
+    return;
+}
+
     
 }

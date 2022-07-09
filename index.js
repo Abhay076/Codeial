2b268132deea5fa99caf3bcec92ff2432e11d6d1
Abhay 076 <yadavabhaycs@gmail.com>
2022-07-09 07:05:57 +0530

    Finished :: Database Relations (Posts,Comments)

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.set('layout extractScripts',true);
  // set up the view engine
  app.set('view engine','ejs');
  app.set('views','./views');
-
+// /
 //mongo store is used to store the session cookie in the db
  app.use(session({
     name: 'codeial',

92091060bb99b5bb24a4fe6a04d83859c955c7d0
Abhay 076 <yadavabhaycs@gmail.com>
2022-07-11 21:36:07 +0530

    playing with api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,6 @@
 const Post = require('../../../models/post');
 
-
+const Comment = require('../../../models/comment');
 module.exports.index = async function(req,res){
 
     let posts = await Post.find({})
@@ -18,4 +18,30 @@ module.exports.index = async function(req,res){
         posts: posts
  
     })
+}
+
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
+       // .id means converting the object id into string
+    // if(post.user==req.user.id){
+        post.remove();
+
+        await Comment.deleteMany({post: req.params.id});
+        
+        return res.status(200).json({
+            message: "Post and associated comments deleted successfully"
+        });
+    // }
+    // else{
+    //     req.flash('error','You cannot delete this post!');
+    //     return res.redirect('back');
+    // }
+    }catch(err){
+         console.log('error',err);
+        return res.status(500).json({
+            message: "internl server error"
+        })
+    }
+   
 }
